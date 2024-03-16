527dded9a86416945bcdd9b6d61cb40ec83c46df
Julien Leleu <julien.leleu@redjista.fr>
2024-03-16 16:13:55 +0100

    Database module for loading data

---
+++ src/infrastructure/database.spec.js
@@ -0,0 +1,19 @@
+import { Database } from './database.js';
+import { describe, expect, it } from '@jest/globals';
+
+describe('Database', () => {
+  it('should load data (countries, peoples and animals)', () => {
+    const countries = Database.findAll();
+
+    expect(countries).toHaveLength(5);
+    expect(countries.some(c => c.name === 'Dillauti')).toBeTruthy();
+
+    const dillauti = countries.find(c => c.name === 'Dillauti');
+    expect(dillauti.people).toHaveLength(5);
+
+    const winifred = dillauti.people.find(p => p.name === 'Winifred Graham');
+    expect(winifred.animals).toHaveLength(6);
+
+    expect(winifred.animals.some(a => a.name === 'Anoa')).toBeTruthy();
+  });
+});
