e32ed281cc81d0266602d8669cc19969c7ecfc60
Julien Leleu <julien.leleu@redjista.fr>
2024-03-16 23:12:27 +0100

    entrypoint

---
+++ app.js
@@ -0,0 +1,20 @@
+import { Database } from './src/infrastructure/database.js';
+import { FilterUseCase } from './src/use-cases/filter/filter.use-case.js';
+import { CountUseCase } from './src/use-cases/count/count.use-case.js';
+import { parseArgs } from './guard.js';
+import { log } from './util.js';
+import { argv } from 'node:process';
+
+function main() {
+  const args = parseArgs(argv.slice(2));
+
+  if (args.error) {
+    console.log(args.error + '\n\n' + args.usage);
+    process.exit(1);
+  }
+
+  if (args.key === '--filter') log(FilterUseCase(Database)(args.value));
+  else if (args.key === '--count') log(CountUseCase(Database)());
+}
+
+main();
