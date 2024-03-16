8de05ed9f17dc1b34d0e6b1b66c98ad7e5a212d2
Julien Leleu <julien.leleu@redjista.fr>
2024-03-16 16:16:34 +0100

    Filter use case

---
+++ src/use-cases/filter/filter.use-case.js
@@ -0,0 +1,15 @@
+export const FilterUseCase = database => pattern => {
+  const countries = database.findAll(pattern);
+
+  const animalMatch = a => new RegExp(pattern, 'i').test(a.name);
+  const peopleWithAnimalMatch = p => p.animals.some(animalMatch);
+  const countryWithPeopleWithAnimalMatch = c => c.people.some(peopleWithAnimalMatch);
+
+  return countries.filter(countryWithPeopleWithAnimalMatch).map(c => {
+    c.people = c.people.filter(peopleWithAnimalMatch).map(p => {
+      p.animals = p.animals.filter(animalMatch);
+      return p;
+    });
+    return c;
+  });
+};
---
+++ src/use-cases/filter/filter.use-case.spec.js
@@ -0,0 +1,68 @@
+import { beforeAll, beforeEach, describe, expect, it } from '@jest/globals';
+import { FilterUseCase } from './filter.use-case.js';
+
+describe('Filter Use Case', () => {
+  let FakeDatabase;
+  let filterAnimals;
+
+  beforeAll(() => {
+    FakeDatabase = {
+      findAll: () => [
+        {
+          name: 'Dillauti',
+          people: [
+            {
+              name: 'Winifred Graham',
+              animals: [{ name: 'Anoa' }, { name: 'Duck' }, { name: 'Narwhal' }]
+            },
+            {
+              name: 'Blanche Viciani',
+              animals: [{ name: 'Barbet' }, { name: 'Rhea' }, { name: 'Snakes' }]
+            }
+          ]
+        },
+        {
+          name: 'Tohabdal',
+          people: [
+            {
+              name: 'Effie Houghton',
+              animals: [{ name: 'Zebra' }, { name: 'Ring-tailed Lemur' }, { name: 'Fly' }]
+            }
+          ]
+        }
+      ]
+    };
+  });
+
+  beforeEach(() => {
+    filterAnimals = FilterUseCase(FakeDatabase);
+  });
+
+  it.each([
+    ['Anoa', 'Dillauti'],
+    ['no', 'Dillauti'],
+    ['Ze', 'Tohabdal'],
+    ['bra', 'Tohabdal']
+  ])('should only return countries containing matching animals', (animal, country) => {
+    expect(filterAnimals(animal)).toHaveLength(1);
+    expect(filterAnimals(animal)[0].name).toEqual(country);
+  });
+
+  it.each([
+    ['Anoa', 'Winifred Graham'],
+    ['no', 'Winifred Graham'],
+    ['Ze', 'Effie Houghton'],
+    ['bra', 'Effie Houghton']
+  ])('should only return people containing matching animals', (animal, people) => {
+    expect(filterAnimals(animal)[0].people).toHaveLength(1);
+    expect(filterAnimals(animal)[0].people[0].name).toEqual(people);
+  });
+
+  it.each([['Anoa'], ['Rhea'], ['Ring-tailed Lemur']])(
+    'should only return matching animals',
+    animal => {
+      expect(filterAnimals(animal)[0].people[0].animals).toHaveLength(1);
+      expect(filterAnimals(animal)[0].people[0].animals[0].name).toEqual(animal);
+    }
+  );
+});
