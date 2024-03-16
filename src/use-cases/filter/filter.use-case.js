26cfed40f7f0cd8cefa97340da9598e22430902c
Julien Leleu <julien.leleu@redjista.fr>
2024-03-16 23:38:51 +0100

    chore: renamed variables

--- src/use-cases/filter/filter.use-case.js
+++ src/use-cases/filter/filter.use-case.js
@@ -1,13 +1,13 @@
 export const FilterUseCase = database => pattern => {
   const countries = database.findAllCountries();
 
-  const animalMatch = a => new RegExp(pattern, 'i').test(a.name);
-  const peopleWithAnimalMatch = p => p.animals.some(animalMatch);
-  const countryWithPeopleWithAnimalMatch = c => c.people.some(peopleWithAnimalMatch);
+  const animalMatchesPattern = a => new RegExp(pattern, 'i').test(a.name);
+  const peopleMatchesAnimal = p => p.animals.some(animalMatchesPattern);
+  const countryMatchesAnimal = c => c.people.some(peopleMatchesAnimal);
 
-  return countries.filter(countryWithPeopleWithAnimalMatch).map(c => {
-    c.people = c.people.filter(peopleWithAnimalMatch).map(p => {
-      p.animals = p.animals.filter(animalMatch);
+  return countries.filter(countryMatchesAnimal).map(c => {
+    c.people = c.people.filter(peopleMatchesAnimal).map(p => {
+      p.animals = p.animals.filter(animalMatchesPattern);
       return p;
     });
     return c;
